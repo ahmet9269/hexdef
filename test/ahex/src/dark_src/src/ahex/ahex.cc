9d103268edd42c777ad18fb2d4c7abd1be1b9dae
ahmet9269 <ahmet.git.acnt@gmail.com>
2025-11-20 11:47:20 +0000

    MW ENV

---
+++ test/ahex/src/dark_src/src/ahex/ahex.cc
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "ahex - Dark Component" << std::endl;
+    return 0;
+}
---
+++ test/ahex/src/white_src/src/ahx/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "ahex - White Component (ahx)" << std::endl;
+    return 0;
+}
